6a0d9440347052200fd4eddc89cf7718d2eb1572
ujkkju117 <ujkkju117@outlook.com>
2024-06-29 19:51:23 +0930

    Share Instant Build

---
+++ Mods/BuildSettings/Plugin.cs
@@ -0,0 +1,26 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+
+namespace InstantBuild;
+
+[BepInPlugin(GUID, PluginName, PluginVersion)]
+[BepInProcess("Digimon World Next Order.exe")]
+public class Plugin : BasePlugin
+{
+    internal const string GUID = "hollofox.DWNO.InstantBuild";
+    internal const string PluginName = "InstantBuild";
+    internal const string PluginVersion = "1.0.0";
+
+    // internal static Config
+    static ManualLogSource Logger = BepInEx.Logging.Logger.CreateLogSource(PluginName);
+    public override void Load() => Harmony.CreateAndPatchAll(typeof(Plugin));
+
+    [HarmonyPatch(typeof(ParameterTownGradeUpData), "GetParam", typeof(uint))]
+    [HarmonyPostfix]
+    public static void GetKindTownGradeUpData_Postfix(ref uint id, ref ParameterTownGradeUpData __result)
+    {
+        if (__result != null) __result.m_grade_up_time = 0;
+    }
+}
--- README.md
+++ README.md
@@ -7,3 +7,7 @@ This mod brings in support for the following screen resolutions:
 - 2560 x 1440
 - 3840 x 2160
 - 3840 x 2400
+
+## Build Settings Plugin
+This mod allows time to build to be instant instead of having to wait an entire day.
+You will still need to visit that part of town to trigger the build, but it will be instant.
